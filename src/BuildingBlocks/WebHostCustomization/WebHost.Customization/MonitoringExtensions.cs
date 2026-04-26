29d3d9850a745b6cd4ce16e4e4767dc753c16f31
Samad Siddiqui <samad.siddiqui91@live.com>
2026-04-26 15:35:48 +0500

    bug fix

--- src/BuildingBlocks/WebHostCustomization/WebHost.Customization/MonitoringExtensions.cs
+++ src/BuildingBlocks/WebHostCustomization/WebHost.Customization/MonitoringExtensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
 using Prometheus;
 
 namespace Microsoft.AspNetCore.Hosting;
